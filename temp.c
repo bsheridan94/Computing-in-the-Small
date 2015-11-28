0a3fea42e045e19eced563d8ac3fbeacfd1cd786
Brian <controler12@gmail.com>
2015-11-27 21:57:53 -0500

    current code for temp sensor

--- temp.c
+++ temp.c
@@ -1,16 +1,135 @@
-#include <u.h>
-#include <lib.c>
+#include<u.h>
+#include<libc.h>
 
 
-void
-main(){
+int resetWire(void);
+void writeWire(int);
+uchar readWire(void);
 
-    int gpio_fd;
 
-    bind("#G", "/dev",MAFTER);
-    gpio_fd = open("/dev/gpio", ORDWR);
+int gpioFD;
 
-    fprint(gpio_fd, "function 25 out");
-    fprint(gpio_fd, "set 25 0");
+//function to reset write
+int resetWire()
+{
+	ulong x;
+    char buf[16];
+	ulong time;
+    time = nsec();
+    //set data line to output
+    fprint(gpioFD, "function 27 out");
+
+    // wait 500 us	
+    while(nsec() < (time + 500000));
+    //set data line to input
+    fprint(gpioFD, "function 27 in");
+
+    // wait 60 us
+	while(nsec() < (time + 560000));
+    read(gpioFD, buf, 16);
+
+    x =  strtoul(buf ,nil, 16);
+
+    if(x &  (1<<27)){
+    	print("Response Received!\n");
+    	return 0;
+    }
+    else{
+    	print("Reset error\n");
+    	return 1;
+    }
+}
+
+
+void writeWire(int cmd){
+    int i;
+    ulong time;
+
+    for(i = 0; i < 8; i++){
+        //Send a 1
+        if(cmd & 0x01){
+            time = nsec();
+            fprint(gpioFD, "function 27 pulse");
+            while(nsec() < (time + 60000));
+        }
+        //Send a 0
+        else{
+            time = nsec();
+            fprint(gpioFD, "function 27 out");
+            while(nsec() < (time + 60000));
+            fprint(gpioFD, "function 27 in");
+            while(nsec() < (time + 80000));
+        }
+        cmd >>= 1;//we move to the next bit
+    }
+}
+
+
+uchar readWire(){
+    ulong x;
+    int i;
+    uchar value;
+    char buf[16];
+    ulong time;
+
+    value = 0;
+
+    for(i = 0; i < 8; i++){
+        time = nsec();
+        fprint(gpioFD, "function 27 pulse");
+
+        x = strtoul(buf, nil, 16);
+        if(x & (1<<27)){
+            value |= (1 << i);
+        }
+        print("%d ", x & (1<<27));
+        while(nsec() < (time + 60000));
+    }
+
+    print("\nresult inside read call: %u\n", value);
+    return value;
+}
+
+
+void main(){
+	int status;
+
+    uchar temp[2];
+    ushort final;
+
+	gpioFD = open("/dev/gpio",ORDWR);
+    if(gpioFD < 0) {
+        bind("#G","/dev",MAFTER);
+        gpioFD = open("/dev/gpio",ORDWR);
+        if(gpioFD < 0)
+        {
+            print("Open Error: %r\n");
+        }
+    }
+    
+    //set pin function and value
+    fprint(gpioFD, "function 27 in");
+    fprint(gpioFD, "set 27 0");
+
+    status = resetWire();
+    sleep(500);
+
+    writeWire(0xCC);
+    writeWire(0x44);
+    sleep(1000);
+
+    status = resetWire();
+    writeWire(0xCC);
+    writeWire(0x44);
+
+    temp[0] = readWire();
+    print("temp[0]: %u\n", temp[0]);
+    temp[1] = readWire();
+    print("temp[1]: %u\n", temp[1]);
+
+    final = temp[1] << 8 | temp[0];
+
+    print("temp: %d\n", final);
 
 }
+
