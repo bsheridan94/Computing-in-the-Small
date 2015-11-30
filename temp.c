8458d869a94b03f17380b3ade004868b625d5191
Brian <controler12@gmail.com>
2015-11-30 11:29:22 -0500

    final temp.c version

--- temp.c
+++ temp.c
@@ -1,11 +1,9 @@
 #include<u.h>
 #include<libc.h>
 
-
 int resetWire(void);
 void writeWire(int);
 uchar readWire(void);
-
 
 int gpioFD;
 
@@ -16,7 +14,6 @@ int resetWire()
     char buf[16];
     long time_nsec;
     long time_elaps;
-
 
     fprint(gpioFD, "function 27 out");
 
@@ -41,7 +38,7 @@ int resetWire()
 
 
     if(!(x &  (1<<27))){
-        print("Response Received!\n");
+        //print("Response Received!\n");
         return 0;
     }
     else{
@@ -62,6 +59,7 @@ void writeWire(int cmd){
             time_nsec = nsec();
             time_elaps = nsec();
             fprint(gpioFD, "function 27 pulse");
+            // transactions should last 80us
             while((time_elaps - time_nsec) < 80000){
                 time_elaps = nsec();
             }
@@ -71,10 +69,12 @@ void writeWire(int cmd){
             time_nsec = nsec();
             time_elaps = nsec();
             fprint(gpioFD, "function 27 out");
+            // waits 60us
             while((time_elaps - time_nsec) < 60000){
                 time_elaps = nsec();
             }
             fprint(gpioFD, "function 27 in");
+            // transactions should last 80us
             while((time_elaps - time_nsec) < 80000){
                 time_elaps = nsec();
             }
@@ -87,13 +87,11 @@ void writeWire(int cmd){
 uchar readWire(){
     ulong x;
     int i;
-    uchar value;
+    uchar ret;
     char buf[16];
-
     long time_nsec;
     long time_elaps;
-
-    value = 0;
+    ret = 0;
 
     //for(i = 7; i > -1; i--){
     for(i = 0; i < 8; i++){
@@ -104,15 +102,14 @@ uchar readWire(){
         buf[8] = 0;
         x = strtoull(buf, nil, 16);
         if(x & (1<<27)){
-            value |= (1 << i);
+            ret |= (1 << i);
         }
+        // transactions should last u0s
         while((time_elaps - time_nsec) < 60000){
                 time_elaps = nsec();
         }
     }
-
-    //print("result inside read call: %d\n", value);
-    return value;
+    return ret;
 }
 
 
@@ -120,8 +117,10 @@ void main(){
     int status;
     int i;
     uchar temp[9];
-    ushort final;
+    ushort finalTemp;
+    int firstTime = 1;
 
+    //Binds GPIO
     gpioFD = open("/dev/gpio",ORDWR);
     if(gpioFD < 0) {
         bind("#G","/dev",MAFTER);
@@ -131,40 +130,51 @@ void main(){
             print("Open Error: %r\n");
         }
     }
-    
-    //set pin function and value
-    fprint(gpioFD, "function 27 in");
-    fprint(gpioFD, "set 27 0");
 
+    while(1){
+         //set pin function and value
+        fprint(gpioFD, "function 27 in");
+        fprint(gpioFD, "set 27 0");
 
-    status = resetWire();
-    sleep(500);
+        status = resetWire();
+        sleep(1000);
 
-    writeWire(0xCC);
-    writeWire(0x44);
-    sleep(1000);
+        //Skip Rom
+        writeWire(0xCC);
+        // Convert Temp
+        writeWire(0x44);
+        sleep(1000);
 
-    status = resetWire();
-    writeWire(0xCC);
-    writeWire(0xBE);
+        status = resetWire();
+        sleep(1000);
 
-    for(i = 0; i < 9; i++) {
-        temp[i] = readWire();
+        //skip Rom
+        writeWire(0xCC);
+        //read scratchPad
+        writeWire(0xBE);
+
+        // read in all 9 bytes of scratchpad
+        for(i = 0; i < 9; i++) {
+            temp[i] = readWire();
+        }
+         
+         // prints whole scratch pad for first run
+        if(firstTime){
+        print("temp[0]: %02x\n", temp[0]);
+        print("temp[1]: %02x\n", temp[1]);
+        print("temp[2]: %02x\n", temp[2]);
+        print("temp[3]: %02x\n", temp[3]);
+        print("temp[4]: %02x\n", temp[4]);
+        print("temp[5]: %02x\n", temp[5]);
+        print("temp[6]: %02x\n", temp[6]);
+        print("temp[7]: %02x\n", temp[7]);
+        print("temp[8]: %02x\n", temp[8]);
+        firstTime = 0;
+        }
+
+        // joins byte 1 and 0 of scratchpad to create temp.
+        finalTemp = temp[1] << 8 | temp[0];
+        print("temp: %d C\n", finalTemp >> 4);
+        sleep(1000);
     }
- 
-    print("temp[0]: %02x\n", temp[0]);
-    print("temp[1]: %02x\n", temp[1]);
-    print("temp[2]: %02x\n", temp[2]);
-    print("temp[3]: %02x\n", temp[3]);
-    print("temp[4]: %02x\n", temp[4]);
-    print("temp[5]: %02x\n", temp[5]);
-    print("temp[6]: %02x\n", temp[6]);
-    print("temp[7]: %02x\n", temp[7]);
-    print("temp[8]: %02x\n", temp[8]);
-
-
-    final = temp[1] << 8 | temp[0];
-
-    print("temp: %d\n", final>>4);
-
 }
